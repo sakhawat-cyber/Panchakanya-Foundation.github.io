6eaa9a08dfccc66dcfb9e2bfc88e9be107602b18
Sakhawat Hossain Sayed <sayed.infacebook.com@gmail.com>
2022-12-26 19:17:45 +0600

    Panchakanya_Foundation

---
+++ fundation-Website-main/js/google-map.js
@@ -0,0 +1,62 @@
+
+var google;
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
+    var myLatlng = new google.maps.LatLng(24.8607, 67.0011);
+    // 39.399872
+    // -8.224454
+    
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 7,
+
+        // The latitude and longitude to center the map (always required)
+        center: myLatlng,
+
+        // How you would like to style the map. 
+        scrollwheel: false,
+        styles: [
+            {
+                "featureType": "administrative.country",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    },
+                    {
+                        "hue": "#ff0000"
+                    }
+                ]
+            }
+        ]
+    };
+
+    
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using out element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+    
+    var addresses = ['Karachi'];
+
+    for (var x = 0; x < addresses.length; x++) {
+        $.getJSON('https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59300.851874114946!2d92.07103429929542!3d21.7297665373188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30ada6addc2ab1c9%3A0x32edda2da9fcbd0b!2sBatakhali%20Masterpara%20Shahi%20Jame%20Masjid!5e0!3m2!1sen!2sbd!4v1655634028677!5m2!1sen!2sbd?address='+addresses[x]+'&sensor=false', null, function (data) {
+            var p = data.results[0].geometry.location
+            var latlng = new google.maps.LatLng(p.lat, p.lng);
+            new google.maps.Marker({
+                position: latlng,
+                map: map,
+                icon: 'images/loc.png'
+            });
+
+        });
+    }
+    
+}
+google.maps.event.addDomListener(window, 'load', init);
